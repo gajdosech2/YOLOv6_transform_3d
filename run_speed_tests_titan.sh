0b15f837e585bf6e875aa2150865101220ff1ae7
Lukas Gajdosech <gajdosech2@saturn.dai.fmph.uniba.sk>
2025-05-15 14:44:50 +0200

    titan script

--- run_speed_tests_titan.sh
+++ run_speed_tests_titan.sh
@@ -11,29 +11,32 @@ echo "Running nano model FP16 480x256..."
 echo "Nano FP16 480x256 finished..."
 
 echo "Running nano model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_nano_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_nano_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Nano FP32 960x544 finished..."
 
 echo "Running nano model FP32 640x352..."
-#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_nano_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_nano_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Nano FP32 640x352 finished..."
 
 echo "Running nano model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_nano_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_nano_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano/yolov6_qa_nano_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Nano FP32 480x256 finished..."
+
+
 
 
 echo "Running nano distill model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_nano_distill_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Nano FP32 960x544 finished..."
+#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_nano_distill_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Nano distill FP32 960x544 finished..."
 
 echo "Running nano distill model FP32 640x352..."
-#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_nano_distill_bb32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Nano FP32 640x352 finished..."
+#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_nano_distill_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Nano distill FP32 640x352 finished..."
 
 echo "Running nano distill model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_nano_distill_bb32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Nano FP32 480x256 finished..."
+#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_nano_distill_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_nano_distill/yolov6_qa_nano_distill_transform_3d.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Nano distill FP32 480x256 finished..."
+
 
 
 
@@ -50,30 +53,31 @@ echo "Running small model FP16 480x256..."
 echo "Small FP16 480x256 finished..."
 
 echo "Running small model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_small_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_small_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Small FP32 960x544 finished..."
 
 echo "Running small model FP32 640x352..."
-python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_small_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_small_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Small FP32 640x352 finished..."
 
 echo "Running small model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_small_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_small_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small/yolov6_qa_small_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Small FP32 480x256 finished..."
+
 
 
 
 echo "Running small distill model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_small_distill_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Small FP32 960x544 finished..."
+#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_small_distill_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Small distill FP32 960x544 finished..."
 
 echo "Running small distill model FP32 640x352..."
-#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_small_diff_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Small FP32 640x352 finished..."
+python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_small_distill_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Small distill FP32 640x352 finished..."
 
 echo "Running small distill model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_small_distill_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
-echo "Small FP32 480x256 finished..."
+#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_small_distill_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_small_distill/yolov6_qa_small_distill_transform_3d.pt.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
+echo "Small distill FP32 480x256 finished..."
 
 
 
@@ -91,16 +95,17 @@ echo "Running medium model FP16 480x256..."
 echo "Medium FP16 480x256 finished..."
 
 echo "Running medium model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_medium_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_medium_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Medium FP32 960x544 finished..."
 
 echo "Running medium model FP32 640x352..."
-#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_medium_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_medium_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Medium FP32 640x352 finished..."
 
 echo "Running medium model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_medium_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_medium_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/qa_medium/yolov6_qa_medium_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Medium FP32 480x256 finished..."
+
 
 
 
@@ -117,16 +122,17 @@ echo "Running large model FP16 480x256..."
 echo "Large FP16 480x256 finished..."
 
 echo "Running large model FP32 960x544..."
-#python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_large_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+python speed_estimation.py --yolo-img-size 544 960 --test-name=yolov6_large_b32_960_544_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Large FP32 960x544 finished..."
 
 echo "Running large model FP32 640x352..."
-#python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_large_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+python speed_estimation.py --yolo-img-size 352 640 --test-name=yolov6_large_b32_640_352_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Large FP32 640x352 finished..."
 
 echo "Running large model FP32 480x256..."
-#python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_large_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Titan/results --processing-batch=32
+python speed_estimation.py --yolo-img-size 256 480 --test-name=yolov6_large_b32_480_256_fp32_titan --model_path=/home/g/gajdosech2/YOLOv6_transform_3d/bcs_trained_models/large/yolov6_large_3d_transform.pt --root_dir_video_path /home/k/kocur15/data/2016-ITS-BrnoCompSpeed/dataset --root_dir_results_path /home/g/gajdosech2/YOLOv6_transform_3d/2016-ITS-BrnoCompSpeed-Fixed/results --processing-batch=32
 echo "Large FP32 480x256 finished..."
+
 
 
 
